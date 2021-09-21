309ae737fefa20056b743a8b9a15ab1070e019c6
Vishnu-Aithal <90306959+Vishnu-Aithal@users.noreply.github.com>
2021-09-21 15:58:38 +0530

    testing- mockserver succes

---
+++ index.js
@@ -0,0 +1,22 @@
+var inputTextBox = document.querySelector("#input-text");
+var outputTextBox = document.querySelector("#output-text");
+var translateBtn = document.querySelector("#translate-btn");
+
+var url = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+// var url = "https://api.funtranslations.com/translate/minion.json"
+
+
+function apiUrlConstructor (text) {
+    return `${url}?text=${text}`
+}
+
+// fetch(apiUrlConstructor("test")).then(responseFromServer => responseFromServer.json()).then(json => console.log(json.contents.translated))
+
+translateBtn.addEventListener("click", clickHandler)
+
+function clickHandler () {
+    var inputText = inputTextBox.value;
+    var apiUrl = apiUrlConstructor(inputText);
+    fetch(apiUrl).then(response => response.json()).then(json => outputTextBox.innerText = json.contents.translated)
+    console.log("clicked");
+}
